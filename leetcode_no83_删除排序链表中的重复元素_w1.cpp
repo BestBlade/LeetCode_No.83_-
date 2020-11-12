fccd380674c40f70fec0b9aea7016178b1291f3f
BestBlade <68151785+BestBlade@users.noreply.github.com>
2020-11-12 11:35:31 +0800

    Add files via upload

---
+++ leetcode_no83_删除排序链表中的重复元素_w1.cpp
@@ -0,0 +1,60 @@
+﻿/* ------------------------------------------------------------------|
+给定一个排序链表，删除所有重复的元素，使得每个元素只出现一次。
+
+示例 1:
+
+输入: 1->1->2
+输出: 1->2
+示例 2:
+
+输入: 1->1->2->3->3
+输出: 1->2->3
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/remove-duplicates-from-sorted-list
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。|
+-------------------------------------------------------------------*/
+
+/*	双指针法
+*
+*	执行用时：0 ms, 在所有 C++ 提交中击败了100.00%的用户
+*	内存消耗：8.1 MB, 在所有 C++ 提交中击败了81.94%的用户
+*/
+
+#include <iostream>
+#include <string> 
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <map>
+#include <stack>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+    
+};
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+ListNode* deleteDuplicates(ListNode* head) {
+    ListNode* node = head;
+
+    while (node && node->next) {
+        if (node->val == node->next->val) {
+            node->next = node->next->next;
+        }
+        else {
+            node = node->next;
+        }
+    }
+    return head;
+}
